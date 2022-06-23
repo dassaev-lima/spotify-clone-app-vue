77a473be85ddcc41de96f51e92d8ce5114a28835
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-23 15:34:31 -0300

    feat: Adicionando a side bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <SideBar/>
     <BarReproduction/>
   </div>
 </template>
 <script>
 import BarReproduction from './components/BarReproduction.vue'
+import SideBar from './components/SideBar.vue'
 export default {
   name: "App",
   data() {
     return {}
   },
   components: {
-    BarReproduction
+    BarReproduction,
+    SideBar
   }
 
 }
