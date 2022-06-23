e7836d15cde1cddcc153853d9d4b89d7e3d212e9
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-23 15:57:11 -0300

    refactor: ocultando a side bar e a barrade de reprodução em dispositivos móveis

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col w-72 h-screen p-6 gap-8" style="background-color:#000">
+    <div class="flex flex-col w-72 h-screen p-6 gap-8 invisible md:visible" style="background-color:#000">
         <ul class="flex flex-col gap-4">
             <li>
                 <img src="../assets/spotify-vue-clone-white.png" alt="" style="heigth=2.5rem;width=100%;max-width:140px;">
