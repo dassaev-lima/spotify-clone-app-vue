a3b7db9b9adaff75978995e593acd3cb2141541f
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-23 13:09:06 -0300

    refactor: componentizano a barra de reprodução e importando no arquivo app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,18 @@
 <template>
-  <div class="w-full bg-stone-900 text-white absolute inset-x-0 bottom-0 h-25 p-4 flex flex-row justify-center items-center">
-    <div class="shrink-0 w-80 flex space-x-4 justify-center items-center">
-      <img src="https://i.scdn.co/image/ab67616d00004851f4e7821b9cda8226bb7eab2c" 
-        data-testid="cover-art-image" alt="" class="object-contain">
-      <div class="flex flex-col space-y-0">
-        <span class="text-sm text-white font-medium">A cruz e o paraíso</span>
-        <span class="text-xs text-gray-300">Fernandinho, Anderson Freire</span>
-      </div>
-      <div class=" flex space-y-0 space-x-2">
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1 ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1 ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-        </span>
-      </div>
-    </div>
-
-    <div class="flex flex-col flex-grow shrink-0 w-80 text-center space-y-4">
-      <div class="flex flex-row flex-nowrap justify-center items-center space-x-4">
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z" />
-          </svg>
-        </span>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-11 w-11 " viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-        </svg>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
-          </svg>
-        </span>
-        
-      </div>
-      <div class="flex flex-row flex-nowrap justify-center items-center">
-        <div class="shrink-0 w-80 flex space-x-4 items-center">
-          <span class="text-xs text-gray-300">0:00</span>
-          <span class="bg-gray-300 h-1.5 rounded-full" style="width: 100%"></span>
-          <span class="text-xs text-gray-300">4:56</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="flex shrink-0 w-80 justify-center items-center">
-      <div class=" flex space-y-0 space-x-2 text-xs text-gray-300">
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1 ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1 ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-        </span>
-        <span class="text-xs text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1 ">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-          </svg>
-        </span>
-        <div class="flex flex-row flex-nowrap justify-center items-center">
-          <div class="shrink-0 w-24 flex space-x-4 items-center">
-            <span class="bg-gray-300 h-1.5 rounded-full" style="width: 100%"></span>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <BarReproduction/>
   </div>
 </template>
 <script>
+import BarReproduction from './components/BarReproduction.vue'
 export default {
   name: "App",
   data() {
     return {}
   },
+  components: {
+    BarReproduction
+  }
 
 }
 </script>
