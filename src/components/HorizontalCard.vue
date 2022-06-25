43fe7e428fc7d8f8d72122815b413f35fb1bad23
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-25 11:07:22 -0300

    fix: melhorando a responsividade dos cards no conteúdo principal

--- src/components/HorizontalCard.vue
+++ src/components/HorizontalCard.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-row grow items-center justify-start space-x-8 h-20 shrink-0 sm:w-full md:w-1/3 lg:w-1/4 rounded-md bg-gradient-to-b from-neutral-500 to-neutral-700">
+    <div class="flex flex-row grow items-center justify-start space-x-8 h-20 shrink-0 sm:w-full md:w-5/6 lg:w-1/4 rounded-md bg-gradient-to-b from-neutral-500 to-neutral-700">
         <img src="https://seed-mix-image.spotifycdn.com/v6/img/romantic/5vXZDaOrGiU7bvoqJiDo0m/pt/default"
         alt="Imagem álbum da música" class="object-fit w-20 rounded-tl-md rounded-bl-md" >
-        <h4 class="text-white font-semibold text-base tracking-wide">Gisele Nascimento</h4>
+        <h4 class="text-white font-semibold text-sm tracking-wide">Gisele Nascimento</h4>
     </div>
 </template>
 <script>
--- src/components/VerticalCard.vue
+++ src/components/VerticalCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-56 h-80 pt-4 pr-4 pl-4 pb-6 flex flex-col bg-card rounded-md space-y-1 grow">
+    <div class="flex flex-col h-auto w-1/2 md:w-1/3 lg:w-56 pt-4 pr-4 pl-4 pb-6 bg-card rounded-md space-y-1 grow ">
             <img src="https://thisis-images.scdn.co/37i9dQZF1DZ06evO0X1Nwj-default.jpg" alt="Anderson Freire" class="rounded-md">
             <span class="text-lg font-semibold text-white pt-4">Stand up</span>
             <span class="text-base font-normal text-stone-400">Stand up</span>
