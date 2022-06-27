d8a1fb14ada528192b04174afeb29c1d8504659b
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-27 15:20:05 -0300

    style: aplicando efeito de hover no ícone de play e no background do card horizontal

--- src/components/HorizontalCard.vue
+++ src/components/HorizontalCard.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="flex flex-row grow items-center justify-start space-x-8 h-20 shrink-0 sm:w-full md:w-5/6 lg:w-1/4 rounded-md bg-gradient-to-b from-neutral-500 to-neutral-700">
+    <div class="flex flex-row grow items-center justify-start space-x-8 h-20 shrink-0 sm:w-full md:w-5/6 lg:w-1/4 
+    rounded-md bg-neutral-700 hover:bg-neutral-500  transition-colors duration-300 pr-4"
+    ref="cardHorizontal"
+    @mouseover="showIconPlay = !showIconPlay" @mouseout="showIconPlay = !showIconPlay">
         <img src="https://seed-mix-image.spotifycdn.com/v6/img/romantic/5vXZDaOrGiU7bvoqJiDo0m/pt/default"
-        alt="Imagem álbum da música" class="object-fit w-20 rounded-tl-md rounded-bl-md" >
-        <h4 class="text-white font-semibold text-sm tracking-wide">Gisele Nascimento</h4>
-        <div class="h-12 w-12 bg-green-500 rounded-full text-black flex justify-center items-center">
+        alt="Imagem álbum da música" class="object-fit w-20 rounded-tl-md rounded-bl-md flex-none" >
+        <h4 class="text-white font-semibold text-sm tracking-wide grow">Gisele Nascimento</h4>
+        <div class="h-12 w-12 bg-green-500 rounded-full text-black flex justify-center items-center shadow-xl hide transition-all">
             <img src="../assets/icon-play.png" alt="Ícon play" class="w-5 h-5">
         </div>
     </div>
@@ -12,7 +15,30 @@
 export default {
     name: "HorizontalCard",
     data() {
-        return {}
+        return {
+            showIconPlay: false
+        }
+    },
+    methods: {
+        toggleIconPlay() {
+            if (this.showIconPlay) {
+                this.$refs.cardHorizontal.children[2].classList.remove("hide");
+            } else {
+                this.$refs.cardHorizontal.children[2].classList.add("hide");        
+            }
+        }
+    },
+    watch: {
+        showIconPlay() {
+            this.toggleIconPlay();
+        }
     }
 }
 </script>
+<style scoped>
+.hide {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0.3s, opacity 0.5s linear;
+}
+</style>
